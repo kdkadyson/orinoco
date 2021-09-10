980d1c7fac5855ce01bf71914a0bceb83e9b3875
kady <kadybsonko@gmail.com>
2021-09-10 18:25:31 +0200

    dernières modifs

--- js/ours.js
+++ js/ours.js
@@ -1,7 +1,7 @@
 
 // FORMAT DE REPRÉSENTATION DES PRODUITS 
 
-class Teddies{  //pr facliter les future entrées
+class Teddies{ //pr facliter les future entrées
     constructor(jsonTeddyItem){  //format renvoyé par le Backend
         jsonTeddyItem && Object.assign(this, jsonTeddyItem);//assigner jsonProduit à la class Teddies
     }// Object.assign permet de récupérer les attributs de jsonProduct sans les écraser mais 1 à 1 pr les mettre ds This qui est une instance de class produit
